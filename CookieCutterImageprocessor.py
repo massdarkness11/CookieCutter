4d543f5713e747bd0b6112476bb6f8584e920b16
Matthias Ho <61093923+massdarkness11@users.noreply.github.com>
2021-11-07 17:07:04 -0500

    Update CookieCutterImageprocessor.py
    added option to resize image to size of original test image. Formatted threshold test to display thresholds in intervals of 5 from 0-255 and allow for chosing the best fit.

--- CookieCutterImageprocessor.py
+++ CookieCutterImageprocessor.py
@@ -3,10 +3,29 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('seaborn')
-file_name = "turtle_for_becca.png"
+default_area = 225625 #475 by 475
+def resize(img, default_area, scale = 100):
+    height, width, channels = img.shape
+    if scale != 100:
+        scale_percent = scale # percent of original size
+    else:
+        scale_percent = default_area/(height*width)* 100
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+  
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return resized
+
+file_name = "Charmander.png"
 file_name_export = file_name[0:-4] + "_final.png"
 
 img = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
+
+img = resize(img, default_area)
+
+
 bgr = img[:,:,:3] # Channels 0..2
 *_, alpha = cv2.split(img)
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -52,19 +71,26 @@ result = np.dstack([brg, alpha])
 #plt.title("clear background Image")
 #plt.show()
 
-#for x in range(int(255/5)):
 
-#    (thresh, blackAndWhiteImage) = cv2.threshold(result, x*5, 255, cv2.THRESH_BINARY)
-#    plt.figure(figsize=(8,8))
-#    plt.imshow(blackAndWhiteImage,cmap="gray")
-#    plt.axis("off")
-#    plt.title(str(x*5) +" threshold background Image")
-#    plt.show()
-(thresh, blackAndWhiteImage) = cv2.threshold(result, 50, 255, cv2.THRESH_BINARY)
+plt.figure(figsize=(200,200))
+#plt.subplots(7,8,False, True)
+for x in range(int(255/5)):
+
+    (thresh, blackAndWhiteImage) = cv2.threshold(result, x*5, 255, cv2.THRESH_BINARY)
+    
+    plt.subplot(7,8,x + 1)
+    plt.imshow(blackAndWhiteImage,cmap="gray")
+    plt.axis("off")
+    plt.title(str(x*5))
+
+plt.show()
+
+threshold = int(input("Desired Threshold between 0 and 225"))
+(thresh, blackAndWhiteImage) = cv2.threshold(result, threshold, 255, cv2.THRESH_BINARY)
 plt.figure(figsize=(8,8))
 plt.imshow(blackAndWhiteImage,cmap="gray")
 plt.axis("off")
-plt.title("50 threshold background Image")
+plt.title(str(threshold) + " threshold background Image")
 plt.show()
 
 
@@ -95,4 +121,4 @@ plt.axis("off")
 plt.title("Export Sketch Image")
 
 plt.show()
-cv2.imwrite(file_name_export, final)
+cv2.imwrite(file_name_export, blackAndWhiteImage)
